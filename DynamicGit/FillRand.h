6c39f1b9177a1d74b34e05f21b3742b540a6c8ae
Александр Ивлев <Ivlev92av@gmail.com>
2021-04-13 16:02:06 +0400

    init

---
+++ DynamicGit/FillRand.h
@@ -0,0 +1,7 @@
+#pragma once
+#include"stdlib.h"
+
+void FillRand(double arr[], const int n);
+template<typename T>void FillRand(T arr[], const int n);
+void FillRand(double** arr, const int m, const int n);
+template<typename T>void FillRand(T** arr, const int m, const int n);
